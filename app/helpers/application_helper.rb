e2a88c0924fd803f81a787786fc77a56bcc37b39
Dan Tran <dvtran@outlook.com>
2013-07-24 10:26:33 -0400

    html5 audio player added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def download_url_for(post_key)  
-    	AWS::S3::S3Object.url_for(post_key, Wisp::Application::BUCKET, :authenticated => false)  
+    	AWS::S3::S3Object.url_for(post_key, Wisp::Application::BUCKET, :authenticated => true)  
 	end  
 end
